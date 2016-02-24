29d7d93377452d2dc272c83ad964b0ade9a14aa3
park-brian <re.public@outlook.com>
2016-02-24 09:55:40 -0500

    process text and add tagged definitions entries found in the glossary

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,9 @@
+var termNames = [];
+
+
 $(document).ready(function() {
+    
+    retrieveTermNames();
     
     $("#term").keypress(function(event) {
         if (event.which == 13) {
@@ -8,6 +13,7 @@ $(document).ready(function() {
     });
     
     $('#search').click(submit);
+    $('#submitText').click(processModule.processText);
 });
 
 function submit() {
@@ -36,6 +42,69 @@ function submit() {
         });
     }    
 }
+
+var processModule = (function() {
+    var enteredText = '';
+    var processedText = '';
+    
+    return {
+        processText: processText
+    };
+    
+    function processText() {
+        processedText = '';
+        
+        var text = $('#sampleText').val().split(' ');
+        var html = '';
+        console.log(text);
+        
+        for (var i = 0; i < text.length; i ++)
+            makeRequest(text[i]);
+        
+    }
+
+
+    function makeRequest(term) {
+    
+        if ($.inArray(term, termNames)) { 
+    
+            var request = $.post('http://' + window.location.hostname + '/glossaryRest/define', 
+            JSON.stringify({ term: term, type: 'exact' }))
+
+            request.done(function(data) {
+                data = JSON.parse(data);
+                
+                console.log(data);
+                
+                if (data.length > 0) {
+                    console.log(term, data);
+                    processedText += '<a href="#" data-toggle="tooltip" data-placement="top" title="' + data[0][2] + '">' + term + '</a> '
+                }
+                
+                else {
+                    console.log(term, ' no data' );
+                    processedText += term + ' ';
+                }
+                
+                console.log(processedText);
+                
+                $('#processedText').html(processedText);
+            });
+        }
+    }
+})();
+
+
+function retrieveTermNames() {
+    $.post('http://' + window.location.hostname + '/glossaryRest/define', 
+    JSON.stringify({ term: '%', type: 'contains' }), function(data) {
+        console.log('Retrieved term names');
+        termNames = JSON.parse(data).map(function(term) { 
+            return term[1]; });
+    });
+}
+
+
 
 function updateResults(term, results) {
     var resultsHTML = '';
